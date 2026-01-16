6c9af46b3fc527eb91471c8ac20218463f60b49a
Ali Haider <alihaider@foomotion.io>
2026-01-16 08:09:30 +0500

    Update Hero component styles for improved layout and spacing

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 function Hero() {
   return (
-    <section className="relative h-screen flex items-center overflow-hidden pt-20">
+    <section className="relative h-screen flex items-center overflow-hidden pt-160 lg:pt-20">
       {/* GRID BG  */}
       <div className="absolute inset-0 bg-gradient-to-br from-background via-muted/5 to-primary/5">
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#f1f5f9_1px,transparent_1px),linear-gradient(to_bottom,#f1f5f9_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] opacity-20"></div>
@@ -71,7 +71,7 @@ function Hero() {
 
               {/* USER TESTIMONIALS */}
               <div className="pt-8">
-                <div className="flex items-center gap-6">
+                <div className="flex items-center gap-18 md:gap-6">
                   {/* USER AVATARS */}
                   <div className="flex -space-x-3">
                     <Image
